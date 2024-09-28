2657124e43c0c023cb32bffdb3871ba94faa7d02
Roel Delos Reyes <badcoder28@gmail.com>
2024-09-28 22:16:06 +0800

    feat(redis): spin up redis instance and configure cache backend to use redis.

--- config/base.py
+++ config/base.py
@@ -180,3 +180,11 @@ CELERY_BEAT_SCHEDULE = {
 
 # refirect url after a successful account creation (login url).
 ACCOUNT_CREATE_SUCCESS_REDIRECT_URL = env("ACCOUNT_CREATE_SUCCESS_REDIRECT_URL")
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": env("REDIS_URL"),
+    }
+}
