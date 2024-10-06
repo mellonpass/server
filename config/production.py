c811513ff521453b9070feb0fe354cf6a375b439
Roel Delos Reyes <badcoder28@gmail.com>
2024-10-07 00:44:52 +0800

    ref(jwtauth): only enable on production

--- config/production.py
+++ config/production.py
@@ -2,4 +2,7 @@ from config.base import *
 
 DEBUG = False
 CORS_ALLOW_ALL_ORIGINS = False
+
+# JWTAuthToken
 JWT_AUTH_ENABLE = True
+JWT_AUTH_PROTECTD_VIEWS = ["mp_graphql.views.mp_graphql_view"]
--- mp_graphql/views.py
+++ mp_graphql/views.py
@@ -14,4 +14,4 @@ class MPGrahpQLView(GraphQLView):
         return view
 
 
-mp_graphql_view = MPGrahpQLView.as_view(schema=schema, allow_queries_via_get=False)
+mp_graphql_view = MPGrahpQLView.as_view(schema=schema, allow_queries_via_get=False, graphql_ide=None)
