f965532096c9f42e10c2000b2032b5ee9c595864
Roel Delos Reyes <badcoder28@gmail.com>
2025-03-05 11:53:09 +0800

    ops(sentry): add environment and adjust traces_sample_rate into 0.1

--- config/production.py
+++ config/production.py
@@ -48,12 +48,11 @@ integrations = [
 
 sentry_sdk.init(
     dsn=env("SENTRY_DSN"),
+    environment=APP_ENVIRONMENT,
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
+    traces_sample_rate=0.1,
     _experiments={
         # Set continuous_profiling_auto_start to True
         # to automatically start the profiler on when
