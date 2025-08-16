702dfa6903cd92559c4f07fec6b10e750be6848e
Roel Delos Reyes <badcoder28@gmail.com>
2025-08-16 14:24:46 +0800

    chore(test user): add RSA OAEP keys for the test user.

--- mp/apps/authx/management/commands/inituser.py
+++ mp/apps/authx/management/commands/inituser.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
+from mp.apps.authx.models import RSAOAEPKey
+
 User = get_user_model()
 
 
@@ -25,4 +27,13 @@ class Command(BaseCommand):
         )
         user.set_password(settings.TEST_USER_LOGIN_HASH)
         user.save(update_fields=["password"])
+
+        RSAOAEPKey.objects.get_or_create(
+            user=user,
+            defaults={
+                "protected_key": settings.TEST_USER_RSA_PROTECTED_PRIVATE_KEY,
+                "public_key": settings.TEST_USER_RSA_PUBLIC_KEY,
+            },
+        )
+
         self.stdout.write(self.style.SUCCESS("Test user %s is set." % user.email))
