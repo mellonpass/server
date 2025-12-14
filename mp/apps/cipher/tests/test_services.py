3bdf2a6df0d2c8cb0d23e139f1d5adc4e1493ed2
Roel Delos Reyes <badcoder28@gmail.com>
2025-12-15 00:16:07 +0800

    fix: handle None data in cipher data builders and raise appropriate errors.
    added tests.

--- mp/apps/cipher/tests/test_services.py
+++ mp/apps/cipher/tests/test_services.py
@@ -4,7 +4,12 @@ import pytest
 from django.conf import settings
 from django.utils import timezone
 
+from mp.apps.cipher.models import SecureNoteType
 from mp.apps.cipher.services import (
+    CipherCardDataBuilder,
+    CipherLoginDataBuilder,
+    CipherSecureNoteDataBuilder,
+    DataBuilderMissingDataError,
     restore_cipher_from_delete_state,
     update_cipher_to_delete_state,
 )
@@ -29,3 +34,25 @@ def test_restore_cipher_from_delete_state():
         owner=cipher.owner, uuid=cipher.uuid
     )
     assert restored_cipher.delete_on is None
+
+
+@pytest.mark.parametrize(
+    "builder_class", [CipherLoginDataBuilder, CipherCardDataBuilder]
+)
+def test_missing_data_builder_raises_error(builder_class):
+    data_builder = builder_class()
+    with pytest.raises(DataBuilderMissingDataError) as exc_info:
+        data_builder.build_cipher_data(data=None)
+
+    object_name = data_builder.__class__.__name__
+    assert (
+        str(exc_info.value)
+        == f"{object_name}'s data cannot be None for building cipher data."
+    )
+
+
+def test_secure_note_data_builder():
+    """Test that SecureNoteDataBuilder won't raise an error if data is not provided."""
+    data_builder = CipherSecureNoteDataBuilder()
+    secure_note = data_builder.build_cipher_data(data=None)
+    assert secure_note.type == SecureNoteType.GENERIC
